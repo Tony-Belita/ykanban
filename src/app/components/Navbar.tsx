8e11aee901b2b803815ba8329e0a011447fa3bd3
Tony-Belita <barryibrahimatalibe@gmail.com>
2025-08-10 19:01:46 +0200

    composant sidebar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,9 @@
+import Dropdown from "./Dropdown";
+import { useState } from "react";
+
+
 export default function Navbar() {
+  const [show, setShow] = useState<boolean>(false);
 
 return (
   <nav className="bg-white border flex h-24">
@@ -13,10 +18,11 @@ return (
 
       <div className="flex items-center space-x-3">
         <button className="bg-blue-500 text-black px-4 py-2 flex rounded-3xl items-center space-x-2">
-           <p>+ Ajouter une tâche</p>
+           <p>+ Ajouter une nouvelle tâche</p>
         </button>
           <div className="flex items-center">
-            <button className="text-3xl mb-4">...</button>
+            <button className="text-3xl mb-4" onClick={() => setShow(!show)}>...</button>
+            <Dropdown show={show} /> {/* rendre la liste déroulante ici et passer show comme prop */}
           </div>
         </div>
       </div>
